7c6a741e099bc483dd53c4a363704e6ed12cb360
Ardit Elezi <ardit.elezi@crystal-system.eu>
2022-11-23 11:40:57 +0100

    Changed inheritance type on User

--- src/main/java/al/crystal/conferenceApp/model/User.java
+++ src/main/java/al/crystal/conferenceApp/model/User.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "user")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "type",
         discriminatorType = DiscriminatorType.STRING)
 @Data
